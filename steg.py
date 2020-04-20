770380a12742cff1571889972bd3245eca5af0d6
Clay Richard <aclayrichard@clayrichard.attlocal.net>
2020-04-20 11:04:34 -0500

    made changes

--- steg.py
+++ steg.py
@@ -1,6 +1,20 @@
 import binascii as askey # ascii library // didnt actually use this 
+from matplotlib import image as mpimg
+from matplotlib import pyplot as plt
+
 
 message = 'type message here' # type message (must be string for the thing to encode correctly )
+pictureFN = 'Emily.png'
 
 
-print(bin(int.from_bytes(message.encode(), 'big'))) # makes it into a 
+# print(bin(int.from_bytes(message.encode(), 'big'))) # makes it into a 
+
+# read in an image file using matplotlib
+image = mpimg.imread(pictureFN)
+image = image*255  # read in png file from folder
+print(image)  # look at the RGB values and get in terms of 0-255 range
+# plt.imshow(image)
+# plt.show()
+
+firstpixel = image[1,1]
+print(firstpixel)
