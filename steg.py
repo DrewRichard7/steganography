d37be5fadab1435ef7e2426dd978e8bc5520f413
Clay Richard <aclayrichard@clayrichard.attlocal.net>
2020-04-20 11:12:40 -0500

    more changes

--- steg.py
+++ steg.py
@@ -11,10 +11,12 @@ pictureFN = 'Emily.png'
 
 # read in an image file using matplotlib
 image = mpimg.imread(pictureFN)
-image = image*255  # read in png file from folder
-print(image)  # look at the RGB values and get in terms of 0-255 range
 # plt.imshow(image)
 # plt.show()
+
+image = image*255  # read in png file from folder
+# print(image)  # look at the RGB values and get in terms of 0-255 range
+
 
 firstpixel = image[1,1]
 print(firstpixel)
