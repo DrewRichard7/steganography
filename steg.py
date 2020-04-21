e3d679e24adf4d76ba5c4cde5975d33d67d7b1f3
Clay Richard <aclayrichard@clayrichard.attlocal.net>
2020-04-20 22:38:35 -0500

    made a lot of changes... come ask me if you have questions

--- steg.py
+++ steg.py
@@ -1,22 +1,41 @@
-import binascii as askey # ascii library // didnt actually use this 
 from matplotlib import image as mpimg
 from matplotlib import pyplot as plt
+import numpy as np
 
 
-message = 'type message here' # type message (must be string for the thing to encode correctly )
+message = 'a' # type message (must be string for the thing to encode correctly )
 pictureFN = 'Emily.png'
 
 
-# print(bin(int.from_bytes(message.encode(), 'big'))) # makes it into a 
+binmsg = bin(int.from_bytes(message.encode(), 'big')) # makes it into a binary
+binmsgar = np.array(binmsg)
+binmsglst = np.ndarray.tolist(binmsgar)
+#binmsglst = binmsglst.astype(np.float)
 
-# read in an image file using matplotlib
-image = mpimg.imread(pictureFN)
+image = mpimg.imread(pictureFN) # read in an image file using matplotlib
 # plt.imshow(image)
 # plt.show()
+image = image*255 # read in png file from folder
+position = 1,1,2
+binaryindex = 2
 
-image = image*255  # read in png file from folder
-# print(image)  # look at the RGB values and get in terms of 0-255 range
+#print(type(image))  # look at the RGB values and get in terms of 0-255 range
+#print(image[position])
+#print(type(binmsgar))
+#print(type(np.float(binmsglst[binaryindex])))
+#print(np.float(binmsglst[binaryindex]))
 
 
-firstpixel = image[1,1]
-print(firstpixel)
+if image[position] % 2 == 0:
+    image[position] = 0
+else:
+    image[position] = 1
+    
+#print(image[position].item())
+#print(type(image[position].item()))
+
+if np.float(binmsglst[binaryindex]) == image[position].item():
+    print('first is the same')
+else:
+    print('not the same')
+
